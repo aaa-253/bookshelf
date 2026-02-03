63179daad4f4b0d109cbfd86d10dbf84e1df67d8
aaa-253 <s253133y@st.u-kochi.ac.jp>
2026-02-03 10:16:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ window.addBook = function() {
         id: Date.now(),
         title: titleInput.value,
         author: authorInput.value || "Unknown",
-        memo: memoInput.value || "",
+        memo: memoInput.value || "No thoughts recorded.",
         emotion: emoInput.value,
         emotionName: emoInput.getAttribute('data-name'),
         rating: parseInt(ratingInput ? ratingInput.value : 3),
@@ -58,14 +58,17 @@ function render() {
     books.forEach((book, index) => {
         const card = document.createElement("div");
         card.className = "book-card";
+        
         const coverContent = book.cover 
             ? `<img src="${book.cover}" class="book-cover-img">`
             : `<div class="no-cover" style="background:${book.emotion}">${book.title[0]}</div>`;
+
         const stars = "★".repeat(book.rating) + "☆".repeat(5 - book.rating);
 
         card.innerHTML = `
             <div class="cover-wrapper" onclick="toggleStatus(${index})">
                 ${coverContent}
+                <div class="memo-overlay">${book.memo}</div>
             </div>
             <div class="book-rating">${stars}</div>
             <div class="book-title">${book.title}</div>
@@ -79,9 +82,10 @@ function render() {
     });
 }
 
-function saveAndRender() {
-    localStorage.setItem("color-log-v4", JSON.stringify(books));
-    render();
+window.toggleStatus = function(index) {
+    const states = ["unread", "reading", "done"];
+    books[index].status = states[(states.indexOf(books[index].status) + 1) % states.length];
+    saveAndRender();
 }
 
 window.deleteBook = function(index) {
@@ -89,4 +93,9 @@ window.deleteBook = function(index) {
         books.splice(index, 1);
         saveAndRender();
     }
+}
+
+function saveAndRender() {
+    localStorage.setItem("color-log-v4", JSON.stringify(books));
+    render();
 }
